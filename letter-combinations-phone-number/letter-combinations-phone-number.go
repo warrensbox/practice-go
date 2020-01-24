f13fd471a951d2cb58a599d5438066980c442b58
warren.veerasingam@gmail.com <warren.veerasingam@gmail.com>
2020-01-23 23:06:37 -0600

    letter combination

---
+++ letter-combinations-phone-number/letter-combinations-phone-number.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	input := "23"
+	output := threeSum(input)
+	fmt.Println(output)
+}
+
+func threeSum(digits string) []string {
+
+	var arrStr []string
+
+	dict := make(map[string]string)
+
+	dict["2"] = "abc"
+	dict["3"] = "def"
+	dict["4"] = "ghi"
+	dict["5"] = "jkl"
+	dict["6"] = "mno"
+	dict["7"] = "pqrs"
+	dict["8"] = "tuv"
+	dict["9"] = "wxyz"
+
+	for _, val := range digits {
+
+		fmt.Println(string(val))
+	}
+
+	return arrStr
+}
