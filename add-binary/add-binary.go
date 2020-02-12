00431004f9f3a8848bbdf49190bccdf081118f4e
warren.veerasingam@gmail.com <warren.veerasingam@gmail.com>
2020-02-11 23:43:49 -0600

    added stages

---
+++ add-binary/add-binary.go
@@ -0,0 +1,42 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	a := "11"
+	b := "1"
+
+	ans := addBinary(a, b)
+
+	fmt.Println(ans)
+}
+
+func addBinary(a string, b string) string {
+
+	return a
+}
+
+// class Solution {
+// 	public String addBinary(String a, String b) {
+// 	  int n = a.length(), m = b.length();
+// 	  if (n < m) return addBinary(b, a);
+// 	  int L = Math.max(n, m);
+
+// 	  StringBuilder sb = new StringBuilder();
+// 	  int carry = 0, j = m - 1;
+// 	  for(int i = L - 1; i > -1; --i) {
+// 		if (a.charAt(i) == '1') ++carry;
+// 		if (j > -1 && b.charAt(j--) == '1') ++carry;
+
+// 		if (carry % 2 == 1) sb.append('1');
+// 		else sb.append('0');
+
+// 		carry /= 2;
+// 	  }
+// 	  if (carry == 1) sb.append('1');
+// 	  sb.reverse();
+
+// 	  return sb.toString();
+// 	}
+//   }
