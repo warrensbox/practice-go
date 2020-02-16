c3d38bc183745bb99fecec3189b95bc344f23a98
warren.veerasingam@gmail.com <warren.veerasingam@gmail.com>
2020-02-16 14:00:13 -0600

    complete binary

--- add-binary/add-binary.go
+++ add-binary/add-binary.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"log"
 	"strconv"
@@ -25,6 +26,19 @@ func getInt(index int, s string) int {
 	return num
 }
 
+func reverseString(str string) string {
+
+	runes := []rune(str)
+
+	for i, j := 0, len(str)-1; i < j; i, j = i+1, j-1 {
+
+		runes[i], runes[j] = runes[j], runes[i]
+
+	}
+
+	return string(runes)
+}
+
 func addBinary(a string, b string) string {
 
 	i := len(a) - 1
@@ -33,17 +47,52 @@ func addBinary(a string, b string) string {
 	fmt.Println("j", j)
 	carry := 0
 	fmt.Println("carry", carry)
-	//var buffer bytes.Buffer
+	var buffer bytes.Buffer
 
 	for i >= 0 || j >= 0 {
+
+		fmt.Println("i", i)
+		fmt.Println("j", j)
 
 		sum := carry
 		fmt.Println("sum", sum)
 
 		if i >= 0 {
 			partA := getInt(i, a)
-			fmt.Println(partA)
+			fmt.Println("partA", partA)
+			sum += partA
+			fmt.Println("sum", sum)
 		}
+
+		if j >= 0 {
+			partB := getInt(j, b)
+			fmt.Println("partB", partB)
+			sum += partB
+			fmt.Println("sum", sum)
+		}
+
+		result := sum % 2
+		fmt.Println("result", result)
+
+		carry = sum / 2
+		fmt.Println("carry", carry)
+
+		buffer.WriteString(strconv.Itoa(result))
+		fmt.Println(buffer.String())
+
+		i--
+		j--
+
+		fmt.Println("--------")
 	}
+
+	// note: need to check if there is any remaining carry
+	if carry != 0 {
+		buffer.WriteString("1")
+	}
+
+	fmt.Println(buffer.String())
+
+	return reverseString(buffer.String())
 
 }
