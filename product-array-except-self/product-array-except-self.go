9116a2e0894163e333a1ee6317c529d5d4d6b54c
warren.veerasingam <warren.veerasingam@gmail.com>
2020-02-18 15:14:42 -0600

    1st iteration

--- product-array-except-self/product-array-except-self.go
+++ product-array-except-self/product-array-except-self.go
@@ -6,9 +6,51 @@ import (
 
 func main() {
 
-	input := []int{5, 7, 7, 8, 8, 10}
-	target := 8
+	input := []int{4, 5, 8, 2}
 
-	output := searchRange(input, target)
+	output := productExceptSelf(input)
+	fmt.Println("output", output)
+}
+
+func productExceptSelf(nums []int) []int {
+
+	N := len(nums)
+
+	leftProduct := make([]int, N)
+	rightProduct := make([]int, N)
+	output := make([]int, N)
+
+	leftProduct[0] = 1
+	rightProduct[N-1] = 1
+
+	for i := 1; i < N; i++ {
+		// fmt.Println("i", i)
+		// fmt.Println("nums", nums[i-1])
+		// fmt.Println("leftProduct", leftProduct[i-1])
+		number := nums[i-1]
+		left := leftProduct[i-1]
+		leftProduct[i] = number * left
+	}
+
+	for i := N - 2; i >= 0; i-- {
+		fmt.Println("i", i)
+		fmt.Println("nums", nums[i+1])
+		fmt.Println("rightProduct", rightProduct[i+1])
+		number := nums[i+1]
+		right := rightProduct[i+1]
+		rightProduct[i] = number * right
+	}
+
+	for i := 0; i < N; i++ {
+		right := rightProduct[i]
+		fmt.Println("right", right)
+		left := leftProduct[i]
+		fmt.Println("left", left)
+		output[i] = right * left
+		fmt.Println(output)
+	}
+
 	fmt.Println(output)
+
+	return output
 }
