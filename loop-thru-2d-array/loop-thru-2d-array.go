3b660af330b364a1084645a6c876a4ec08977ca5
warren.veerasingam <warren.veerasingam@gmail.com>
2020-02-20 12:08:35 -0600

    added range

--- loop-thru-2d-array/loop-thru-2d-array.go
+++ loop-thru-2d-array/loop-thru-2d-array.go
@@ -13,14 +13,28 @@ func main() {
 	}
 
 	fmt.Println("length", len(input))
-	fmt.Println(input)
-
 	for i := 0; i < len(input); i++ {
-		//fmt.Println(input[i])
 		for j := 0; j < len(input[i]); j++ {
 			fmt.Print(input[i][j])
 		}
-		fmt.Println("")
+		fmt.Println() //print new line
+	}
+
+	subset(input, 2, 1, 4, 3)
+
+}
+
+func subset(input [][]int, row1 int, col1 int, row2 int, col2 int) {
+
+	fmt.Println(input)
+
+	for i := row1; i <= row2; i++ {
+		//fmt.Println(input[i])
+
+		for j := col1; j <= col2; j++ {
+			fmt.Print(input[i][j])
+		}
+		fmt.Println()
 	}
 
 }
