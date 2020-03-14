239df1e169aea312125333ac0b22a686b53b3139
warren.veerasingam@gmail.com <warren.veerasingam@gmail.com>
2020-03-14 13:17:56 -0500

    add string

--- add-strings/add-strings.go
+++ add-strings/add-strings.go
@@ -5,9 +5,9 @@ import "fmt"
 func main() {
 
 	num1 := "56"
-	num2 := "40"
+	num2 := "140"
 
-	output := addStrings(num1)
+	output := addStrings(num1, num2)
 
 	fmt.Println(output)
 
@@ -28,5 +28,33 @@ func addStrings(num1 string, num2 string) string {
 
 	carry := 0
 	k := len(ans) - 1
+	for lengthNum1 >= 0 || lengthNum2 >= 0 {
+		sum := carry
+		if lengthNum1 >= 0 {
+			sum += int(num1[lengthNum1] - '0')
+			lengthNum1--
+		}
+		if lengthNum2 >= 0 {
+			sum += int(num2[lengthNum2] - '0')
+			lengthNum2--
+		}
+
+		if sum >= 10 {
+			carry = 1
+			sum -= 10
+		} else {
+			carry = 0
+		}
+
+		ans[k] = byte(sum + '0')
+		k--
+	}
+
+	if carry == 1 {
+		ans[k] = byte(1 + '0')
+		k--
+	}
+
+	return string(ans[k+1:])
 
 }
