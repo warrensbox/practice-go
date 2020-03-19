2f431cff07791271fb0f7f132818d9976e1c6077
warren.veerasingam@gmail.com <warren.veerasingam@gmail.com>
2020-03-18 23:59:20 -0500

    cd

---
+++ verifying-an-alien-dictionary/verifying-an-alien-dictionary.go
@@ -0,0 +1,70 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	//words := []string{"hello", "leetcode"} //true
+	//words := []string{"word", "world", "row"} //false
+	words := []string{"apple", "app"} //false
+
+	//order := "hlabcdefgijkmnopqrstuvwxyz"//true
+	//order := "worldabcefghijkmnpqstuvxyz" //false
+	order := "abcdefghijklmnopqrstuvwxyz" //false
+
+	output := isAlienSorted(words, order)
+
+	fmt.Println(output)
+
+}
+
+func isAlienSorted(words []string, order string) bool {
+
+	charMap := [26]int{}
+
+	for i := 0; i < len(order); i++ {
+		charMap[order[i]-'a'] = i
+	}
+
+	for i := 1; i < len(words); i++ {
+		if !compare(words[i-1], words[i], charMap) {
+			return false
+		}
+	}
+
+	return true
+}
+
+func compare(word1, word2 string, charMap [26]int) bool {
+
+	if len(word1) == 0 {
+		return true
+	}
+
+	if len(word2) == 0 {
+		return false
+	}
+
+	for i := 0; i < len(word1) && i < len(word2); i++ {
+
+		charAtWord1 := word1[i]
+		charAtWord2 := word2[i]
+		if charAtWord1 == charAtWord2 {
+			continue
+		}
+
+		if charAtWord1 != charAtWord1 {
+			if charMap[charAtWord1-'a'] > charMap[charAtWord2-'a'] {
+				return false
+			}
+		}
+
+	}
+
+	if len(word1) > len(word2) {
+		return false
+	}
+
+	return true
+
+}
