08b4e4f291b56be3258c9520b3fc5f500930d86a
warren.veerasingam <warren.veerasingam@gmail.com>
2020-03-25 23:56:19 -0500

    added buls and cow

---
+++ bulls-and-cows/bulls-and-cows.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+
+func main() {
+	secret := "1807"
+	guess := "7810"
+
+	output := getHint(secret, guess)
+
+	fmt.Println(output)
+}
+
+func getHint(secret string, guess string) string {
+
+	tmp := make([]int, 10)
+	cow := 0
+	bull := 0
+	for i := 0; i < len(guess); i++ {
+
+		g := guess[i] - '0'
+		s := secret[i] - '0'
+
+		if g == s {
+			bull++
+			continue
+		}
+
+		if tmp[s] < 0 {
+			cow++
+		}
+		if tmp[g] > 0 {
+			cow++
+		}
+		tmp[s]++
+		tmp[g]--
+	}
+
+	return fmt.Sprintf("%vA%vB", bull, cow)
+}
