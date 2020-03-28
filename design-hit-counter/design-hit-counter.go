e33ccb35e541174f94644861fb1fb235a38e1c71
warren.veerasingam <warren.veerasingam@gmail.com>
2020-03-28 16:16:23 -0500

    added hitt counter stuff

---
+++ design-hit-counter/design-hit-counter.go
@@ -0,0 +1,69 @@
+package main
+//disclaimer - I have no idea how this works for now
+import "fmt"
+
+func main() {
+
+	/**
+	 * Your HitCounter object will be instantiated and called as such:
+	 * obj := Constructor();
+	 * obj.Hit(timestamp);
+	 * param_2 := obj.GetHits(timestamp);
+	 */
+
+	obj := Constructor()
+	// obj.Hit()
+	// obj.Hit(2)
+	param_2 := obj.GetHits(301)
+
+	fmt.Println(param_2)
+}
+
+const size = 300
+type HitCounter struct {
+	hits  []int
+	total int
+	start int
+}
+
+/** Initialize your data structure here. */
+func Constructor() HitCounter {
+	return HitCounter{
+		hit : make([]int,size)
+	}
+}
+
+func (this *HitCounter) clear(slots int, t int){
+	if slots <= 0 {
+		return
+	}
+
+	if slots > size {
+		slots = size
+	}
+
+	for i:=0; i < slots; i++{
+		this.total -= this.hits[this.start % size]
+		this.hits[this.start% size] = 0
+		this.start = this.start -1
+	}
+
+	if slots == size {
+		this.start = t-(t%size)
+	}
+}
+
+/** Record a hit.
+  @param timestamp - The current timestamp (in seconds granularity). */
+func (this *HitCounter) Hit(timestamp int) {
+	this.clear(timestamp - size - this.start, timestamp)
+    this.hits[(timestamp - 1) % size]++
+    this.total++
+}
+
+/** Return the number of hits in the past 5 minutes.
+  @param timestamp - The current timestamp (in seconds granularity). */
+func (this *HitCounter) GetHits(timestamp int) int {
+	this.clear(timestamp-size-this.start, timestamp)
+    return this.total
+}
---
+++ my-calendar-I/my-calendar-I.go
@@ -0,0 +1,22 @@
+package main
+
+/**
+ * Your MyCalendar object will be instantiated and called as such:
+ * obj := Constructor();
+ * param_1 := obj.Book(start,end);
+ */
+
+func main() {
+	obj := Constructor()
+}
+
+type MyCalendar struct {
+}
+
+func Constructor() MyCalendar {
+
+}
+
+func (this *MyCalendar) Book(start int, end int) bool {
+
+}
