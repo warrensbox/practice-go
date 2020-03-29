14dd1d4a03064d52e9db1bc062ab892aae01b84c
warren.veerasingam@gmail.com <warren.veerasingam@gmail.com>
2020-03-29 00:20:50 -0500

    added my calendar

--- my-calendar-I/my-calendar-I.go
+++ my-calendar-I/my-calendar-I.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 /**
  * Your MyCalendar object will be instantiated and called as such:
  * obj := Constructor();
@@ -8,15 +10,29 @@ package main
 
 func main() {
 	obj := Constructor()
+	param := obj.Book(3, 5)
+	param = obj.Book(1, 4)
+	fmt.Println(param)
 }
 
 type MyCalendar struct {
+	Calendar [][]int
 }
 
 func Constructor() MyCalendar {
-
+	return MyCalendar{}
 }
 
 func (this *MyCalendar) Book(start int, end int) bool {
 
+	fmt.Println("this.Calendar", this.Calendar)
+
+	for i := 0; i < len(this.Calendar); i++ {
+		if start < this.Calendar[i][1] && end > this.Calendar[i][0] {
+			return false
+		}
+	}
+
+	this.Calendar = append(this.Calendar, []int{start, end})
+	return true
 }
