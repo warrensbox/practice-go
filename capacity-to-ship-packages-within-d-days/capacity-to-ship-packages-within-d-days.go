b4c86a1adab2071c2d2e24e1667ddfa1229f2692
warren.veerasingam <warren.veerasingam@gmail.com>
2020-04-07 18:59:12 -0500

    ship capacity

---
+++ capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.go
@@ -0,0 +1,79 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	weights := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	D := 5
+
+	output := shipWithinDays(weights, D)
+
+	fmt.Println(output)
+
+}
+
+func shipWithinDays(weights []int, D int) int {
+
+	lo := maximumOfWeights(weights)
+	hi := sum(weights)
+	fmt.Println(lo)
+	fmt.Println(hi)
+
+	for lo < hi {
+		fmt.Println("-----")
+		fmt.Println(lo)
+		fmt.Println(hi)
+
+		days := 1
+		load := 0
+		capacity := lo + (hi-lo)/2
+		fmt.Println("capacity", capacity)
+		for i := 0; i < len(weights); i++ {
+			fmt.Println("=====")
+			fmt.Println("weights[i]", weights[i])
+			fmt.Println("load", load)
+			if weights[i]+load > capacity {
+				load = weights[i]
+				days++
+				fmt.Println("days", days)
+			} else {
+				fmt.Println("w else")
+				load += weights[i]
+				fmt.Println("LOAD", load)
+			}
+		}
+
+		if days <= D {
+			hi = capacity
+		} else {
+			lo = capacity + 1
+		}
+	}
+
+	return lo
+}
+
+func sum(weights []int) int {
+
+	sum := 0
+
+	for i := 1; i < len(weights); i++ {
+		sum += weights[i]
+	}
+
+	return sum
+}
+
+func maximumOfWeights(weights []int) int {
+
+	max := weights[0]
+
+	for i := 1; i < len(weights); i++ {
+		if weights[i] > max {
+			max = weights[i]
+		}
+	}
+
+	return max
+}
