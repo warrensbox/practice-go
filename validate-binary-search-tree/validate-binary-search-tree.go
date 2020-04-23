30bd38a76e4ff955901f2195cd16326d823aa278
warren.veerasingam@gmail.com <warren.veerasingam@gmail.com>
2020-04-22 23:56:43 -0500

    undo type

--- validate-binary-search-tree/validate-binary-search-tree.go
+++ validate-binary-search-tree/validate-binary-search-tree.go
@@ -6,6 +6,15 @@
  *     Right *TreeNode
  * }
  */
+
+
+type TreeNode struct {
+     Val int
+     Left *TreeNode
+     Right *TreeNode
+ }
+
+
  func isValidBST(root *TreeNode) bool {
     return helper(root,math.MinInt64, math.MaxInt64)
 }
