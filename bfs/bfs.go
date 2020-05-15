bd17f659b260cfd4e43336a6776ae379da5e9bd0
warren.veerasingam@gmail.com <warren.veerasingam@gmail.com>
2020-05-15 00:00:27 -0500

    .

--- bfs/bfs.go
+++ bfs/bfs.go
@@ -2,4 +2,5 @@
 
 //https://cybernetist.com/2019/03/09/breadth-first-search-using-go-standard-library/
 
-//https://www.youtube.com/watch?v=p9yZNLeOj4s
+
+https://www.youtube.com/watch?v=p9yZNLeOj4s
