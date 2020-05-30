fd5dface920101cf460d48c8970a98cac41e5ed2
warren.veerasingam <warren.veerasingam@gmail.com>
2020-05-29 19:35:04 -0500

    flatten linked list

---
+++ flatten-a-multilevel-doubly-linked-list/flatten-a-multilevel-doubly-linked-list.go
@@ -0,0 +1,42 @@
+/**
+ * Definition for a Node.
+ * type Node struct {
+ *     Val int
+ *     Prev *Node
+ *     Next *Node
+ *     Child *Node
+ * }
+ */
+
+type Node struct {
+  Val int
+  Prev *Node
+  Next *Node
+  Child *Node
+}
+
+
+ func flatten(root *Node) *Node {
+	 if root == nil {
+		 return nil
+	 }
+	 prev := &Node{Val:-1}
+	 stack := []*Node{root}
+
+	 for len(stack) >0 {
+		 curr := stack[len(stack)-1]
+		 stack = stack[:len(stack)-1]
+		 curr.Prev = prev
+		 prev.Next = curr
+		 prev = curr
+		 if curr.Next != nil {
+			 stack = append(stack, curr.Next)
+		 }
+		 if curr.Child != nil {
+			stack = append(stack, curr.Child) 
+			curr.Child = nil
+		 }
+	 }
+	 root.Prev = nil
+	 return root
+ }
