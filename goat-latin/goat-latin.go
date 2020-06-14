bb42b6cbc531adcb58f04dd791b438a3b5880864
warren.veerasingam <warren.veerasingam@gmail.com>
2020-06-14 13:48:27 -0500

    goat latin

---
+++ goat-latin/goat-latin.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	output := toGoatLatin("I speak Goat Latin")
+	//toGoatLatin("The quick brown fox jumped over the lazy dog")
+
+	fmt.Println(output)
+}
+
+func toGoatLatin(S string) string {
+	var result strings.Builder
+	words := strings.Fields(S)
+	add := "a"
+	for _, word := range words {
+
+		fmt.Println(word[:1])
+		switch word[:1] {
+		case "a", "e", "i", "o", "u", "A", "E", "I", "O", "U":
+			word = word + "ma"
+		default:
+			word = word[1:] + word[:1] + "ma"
+		}
+		fmt.Println(word + add)
+		result.WriteString(word + add + " ")
+		add += "a"
+
+	}
+	return result.String()
+}
