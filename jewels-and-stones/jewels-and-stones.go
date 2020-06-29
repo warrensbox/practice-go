c7735f30496e48c41d14a1f866d6504565141418
warren.veerasingam@gmail.com <warren.veerasingam@gmail.com>
2020-06-28 23:50:43 -0500

    complete

--- jewels-and-stones/jewels-and-stones.go
+++ jewels-and-stones/jewels-and-stones.go
@@ -15,6 +15,19 @@ func main() {
 func numJewelsInStones(J string, S string) int {
 
 	count := 0
+	dict := make(map[rune]bool)
+
+	for _, val := range J {
+
+		dict[val] = true
+	}
+
+	for _, val := range S {
+
+		if ok, _ := dict[val]; ok {
+			count++
+		}
+	}
 
 	return count
 
