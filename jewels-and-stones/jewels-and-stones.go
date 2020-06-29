8aec004fd9166ed84a23bf58f477bf8916b034a6
warren.veerasingam@gmail.com <warren.veerasingam@gmail.com>
2020-06-28 23:46:36 -0500

    jewels-and-stones

---
+++ jewels-and-stones/jewels-and-stones.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	J := "aA"
+	S := "aAAbbbb"
+
+	output := numJewelsInStones(J, S)
+	fmt.Println(output)
+}
+
+func numJewelsInStones(J string, S string) int {
+
+	count := 0
+
+	return count
+
+}
