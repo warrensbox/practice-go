c96b98f5b699a01dcbb34a327879fc1e30202438
warren.veerasingam@gmail.com <warren.veerasingam@gmail.com>
2020-07-09 23:31:41 -0500

    preorder tranversal

--- n-ary-tree-preorder-traversal/n-ary-tree-preorder-traversal.go
+++ n-ary-tree-preorder-traversal/n-ary-tree-preorder-traversal.go
@@ -1,1 +1,32 @@
-https://www.youtube.com/watch?v=BHB0B1jFKQc&t=138s
+//https://www.youtube.com/watch?v=BHB0B1jFKQc&t=138s
+
+/**
+ * Definition for a Node.
+ * type Node struct {
+ *     Val int
+ *     Children []*Node
+ * }
+ */
+
+func preorder(root *Node) []int {
+
+	res := make([]int, 0)
+
+	iterate(root, &res)
+
+	return res
+
+}
+
+func iterate(root *Node, res *[]int) {
+
+	if root == nil {
+		return
+	}
+
+	*res = append(*res, root.Val)
+	for _, val := range root.Children {
+
+		iterate(val, res)
+	}
+}
