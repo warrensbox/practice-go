39f833401f00a64fb64952a997a8c42884b3eb59
warren.veerasingam <warren.veerasingam@gmail.com>
2020-07-18 00:15:44 -0500

    solved problem

--- container-with-most-water/container-with-most-water.go
+++ container-with-most-water/container-with-most-water.go
@@ -1,7 +1,14 @@
 package main
 
+import "fmt"
+
 func main() {
 
+	input := []int{1, 8, 6, 2, 5, 4, 8, 3, 7}
+
+	output := maxArea(input)
+
+	fmt.Println(output)
 }
 
 func maxArea(height []int) int {
@@ -12,7 +19,31 @@ func maxArea(height []int) int {
 
 	for left < right {
 
+		maxArea = max(maxArea, min(height[left], height[right])*(right-left))
+
+		if height[left] < height[right] {
+			left++
+		} else {
+			right--
+		}
+
 	}
 
 	return maxArea
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+
+	return b
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+
+	return b
 }
