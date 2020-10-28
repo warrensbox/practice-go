dd2b4e7be411255ad3f386d72d1bddc317ed2870
warren.veerasingam <warren.veerasingam@gmail.com>
2020-10-28 11:47:25 -0500

    remove dups

--- cracking-the-code/linked-lists/2.1-removeDups/removeDups.go
+++ cracking-the-code/linked-lists/2.1-removeDups/removeDups.go
@@ -1,58 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"container/list"
+)
 
 func main() {
 
-	//listExist := make(map[int]bool)
+	list := list.New()
 
-	obj := Constructor()
+	list.PushBack(1)
+	list.PushBack(2)
+	list.PushBack(3)
+	list.PushBack(1)
 
-	obj.AddWord("hello")
+	previous := *list
+	head := list.Front()
 
-}
+	for list != nil {
 
-type WordDictionary struct {
-	root *Node
-}
-
-type Node struct {
-	isEnd bool
-	links map[rune]*Node
-}
-
-/** Initialize your data structure here. */
-func Constructor() WordDictionary {
-	return WordDictionary{
-		root: &Node{
-			links: make(map[rune]*Node),
-		},
 	}
-}
-
-/** Adds a word into the data structure. */
-func (this *WordDictionary) AddWord(word string) {
-	curr := this.root
-	for _, r := range word {
-		fmt.Println("string(r)", string(r))
-		node, ok := curr.links[r]
-		fmt.Println("ok", ok)
-		if !ok {
-			curr.links[r] = &Node{
-				links: make(map[rune]*Node),
-			}
-			curr = curr.links[r]
-		} else {
-			curr = node
-		}
-	}
-	curr.isEnd = true
-}
-
-/** Adds a word into the data structure. */
-func (this *WordDictionary) PrintAll() {
-	curr := this.root
-
-	fmt.Println()
-	curr.isEnd = true
 }
