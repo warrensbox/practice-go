f637731da2f8d0b9db767f54d853c24d90c61ef5
warren.veerasingam <warren.veerasingam@gmail.com>
2020-10-29 23:58:53 -0500

    revert back

--- cracking-the-code/linked-lists/2.1-removeDups/removeDups.go
+++ cracking-the-code/linked-lists/2.1-removeDups/removeDups.go
@@ -2,10 +2,14 @@ package main
 
 import (
 	"container/list"
+	"fmt"
 )
 
 func main() {
 
+	dict := make(map[interface{}]bool)
+
+	previous := list.Element{}
 	list := list.New()
 
 	list.PushBack(1)
@@ -13,10 +17,32 @@ func main() {
 	list.PushBack(3)
 	list.PushBack(1)
 
-	previous := *list
-	head := list.Front()
+	node := list.Front()
 
-	for list != nil {
+	for node != nil {
+		if _, ok := dict[node.Value]; ok {
+			previous.Next().Value = node.Next()
+		} else {
+			dict[node.Value] = true
+			previous = *node
+		}
 
+		//fmt.Println(e.Value)
+
+		//break
+		node = node.Next()
 	}
+
+	n := list.Front()
+
+	for list.Len() > 0 {
+		for n != nil {
+
+			fmt.Println(n.Value)
+			//l.queueCat.Remove(e) // Dequeue
+			//break
+			n = n.Next()
+		}
+	}
+
 }
