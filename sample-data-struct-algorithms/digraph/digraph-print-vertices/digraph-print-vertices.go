d3e86a55890938dfe4cf7728fee8445968c07992
warren.veerasingam <warren.veerasingam@gmail.com>
2020-12-12 23:59:34 -0600

    added vw

--- sample-data-struct-algorithms/digraph/digraph-print-vertices/digraph-print-vertices.go
+++ sample-data-struct-algorithms/digraph/digraph-print-vertices/digraph-print-vertices.go
@@ -1,5 +1,21 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/warrensbox/practice-go/sample-data-struct-algorithms/libsample"
+)
+
 func main() {
 
+}
+
+func Print(g *libsample.Graph) {
+
+	for v := 0; v < g.NumofVertices(); v++ {
+		w_adj := g.Adjacent(v)
+		for w := range w_adj {
+			fmt.Printf(%v "->" %v,v,w)
+		}
+	}
 }
