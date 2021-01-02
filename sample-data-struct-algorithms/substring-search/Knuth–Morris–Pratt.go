cbc5f532a225b419204d0a7ab5b368aeb3727fab
warren.veerasingam <warren.veerasingam@gmail.com>
2021-01-02 12:17:05 +0800

    kmp

--- sample-data-struct-algorithms/substring-search/Knuth–Morris–Pratt.go
+++ sample-data-struct-algorithms/substring-search/Knuth–Morris–Pratt.go
@@ -1,1 +1,26 @@
 package main
+
+func main() {
+
+}
+
+type pat string
+
+func (this *pat) KMP(pattern string) {
+
+	this.pat = pattern
+
+}
+
+// public KMP(String pat)
+// {
+//  this.pat = pat;
+//  M = pat.length();
+//  dfa = new int[R][M];
+//  dfa[pat.charAt(0)][0] = 1;
+//  for (int X = 0, j = 1; j < M; j++)
+//  {
+//  for (int c = 0; c < R; c++)
+//  dfa[c][j] = dfa[c][X];
+//  dfa[pat.charAt(j)][j] = j+1;
+//  X = dfa[pat.charAt(j)][X];
