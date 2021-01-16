8a767d7b452b41866c5a11d25f01197a0f04e740
warren.veerasingam <warren.veerasingam@gmail.com>
2021-01-16 23:24:01 +0800

    tree node

--- symmetric-tree/symmetric-tree.go
+++ symmetric-tree/symmetric-tree.go
@@ -6,23 +6,25 @@
  *     Right *TreeNode
  * }
  */
- func isSymmetric(root *TreeNode) bool {
-    
-    
-    return mirror(root,root)
-    
+
+//NEED REVIEW
+//ADD tre node
+
+func isSymmetric(root *TreeNode) bool {
+
+	return mirror(root, root)
+
 }
 
-
 func mirror(node1 *TreeNode, node2 *TreeNode) bool {
-    
-    if (node1 == nil && node2 == nil ) {
-        return true
-    }
-    
-    if (node1 == nil || node2 == nil ) {
-        return false
-    }
-    
-    return (node1.Val == node2.Val) && mirror(node1.Left,node2.Right) && mirror(node1.Right,node2.Left)
+
+	if node1 == nil && node2 == nil {
+		return true
+	}
+
+	if node1 == nil || node2 == nil {
+		return false
+	}
+
+	return (node1.Val == node2.Val) && mirror(node1.Left, node2.Right) && mirror(node1.Right, node2.Left)
 }
