02b4d56053959e500393043cd21bc02d134d2052
warren.veerasingam <warren.veerasingam@gmail.com>
2021-02-04 23:46:55 +0800

    hanoi

---
+++ cracking-the-code/recursion-dynamic-programming/8.5-Tower-of-Hanoi/Tower-of-Hanoi.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	hanoi(3, 1, 3)
+}
+
+func hanoi(n, start, end int) {
+	if n == 1 {
+		pm(start, end)
+		return
+	}
+	other := 6 - (start + end)
+	hanoi(n-1, start, other)
+	pm(start, end)
+	hanoi(n-1, other, end)
+}
+
+func pm(start, end int) {
+	fmt.Printf("%v -> %v\n", start, end)
+}
