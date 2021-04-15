11e21d8aed48532d812dd40ebece90883197c1c5
warren.veerasingam <warren.veerasingam@gmail.com>
2021-04-14 20:13:13 -0500

    anagram

---
+++ find-all-anagrams-in-a-string/sol/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+
+	s := "cbaebabacd"
+	p := "abc"
+
+	output := findAnagrams(s, p)
+
+	fmt.Println(output)
+}
+
+func findAnagrams(s string, p string) []int {
+
+	ans := []int{}
+	hash := [26]int{}  //s
+	phash := [26]int{} //p
+
+	windowofP := len(p)
+	lenOfS := len(s)
+
+	for lenOfS < windowofP {
+		return ans
+	}
+	left, right := 0, 0
+	for right < windowofP {
+		phash[p[right]-'a']++ //p
+		hash[s[right]-'a']++  //s
+		right++
+	}
+	right-- // bring it back (increased it previously)
+
+	fmt.Println("right", right)
+	// fmt.Println("hash", hash)
+	for right < lenOfS { //looping through s
+
+		if phash == hash {
+			ans = append(ans, left)
+		}
+		right++
+		fmt.Println("ans", ans)
+		os.Exit(0)
+	}
+
+	return ans
+}
