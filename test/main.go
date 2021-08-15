c8cac566d8f8d3a0e0bc5f91939b3e5b91c771b5
warren.veerasingam <warren.veerasingam@gmail.com>
2021-08-15 01:01:45 -0500

    .

--- test/main.go
+++ test/main.go
@@ -1,19 +1,74 @@
+// To execute Go code, please declare a func main() in a package "main"
+
 package main
 
 import (
 	"fmt"
-	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
-	// Outputs your 'visual' env var (in my case 'vim' (set in ~/.bashrc))
-	fmt.Println(os.Getenv("VISUAL"))
 
-	// Create your own environment variable
-	os.Setenv("SITE", "GoLangCode")
-	fmt.Println(os.Getenv("SITE"))
+	str := "3[a2[cd]ef]]"
+	//str := "2[abc]3[cd]ef"
+	//str := "3[a2[c]]"
+	//str := "3[a]2[bc]"
+	fmt.Println(decodeString(str))
+}
 
-	// Equals empty string if doesn't exist
-	fmt.Println(os.Getenv("missing"))
+/*
+goal: decode string according to num[char]
+
+
+*/
+
+func decodeString(s string) string {
+
+	stack := []string{}
+	curr := ""
+	for _, val := range s {
+		if string(val) != "]" {
+			stack = append(stack, string(val)) //push
+		} else {
+
+			for len(stack) > 0 {
+				//top
+				top := stack[len(stack)-1]
+				//pop
+				stack = stack[:len(stack)-1]
+				if top != "[" {
+					curr = top + curr
+				} else if top == "[" {
+					kStr := ""
+					for len(stack) > 0 {
+						// peek
+						n := len(stack) - 1
+						top := stack[n]
+						if _, err := strconv.Atoi(top); err == nil {
+							kStr = top + kStr
+							// pop
+							stack = stack[:n]
+						} else {
+							break
+						}
+					}
+
+					i, _ := strconv.Atoi(kStr)
+					curr = strings.Repeat(curr, i)
+					stack = append(stack, curr)
+					curr = ""
+					break
+				}
+			}
+			stack = append(stack, curr)
+		}
+	}
+
+	var res strings.Builder
+	for _, s := range stack {
+		res.WriteString(s)
+	}
+	return res.String()
 
 }
