9a10eb7f37fa7cf7b96d9ba695707b1983715b51
warren.veerasingam <warren.veerasingam@gmail.com>
2021-08-18 18:37:13 -0500

    test

--- test/main.go
+++ test/main.go
@@ -4,71 +4,53 @@ package main
 
 import (
 	"fmt"
-	"strconv"
-	"strings"
 )
 
 func main() {
 
-	str := "3[a2[cd]ef]]"
-	//str := "2[abc]3[cd]ef"
-	//str := "3[a2[c]]"
-	//str := "3[a]2[bc]"
-	fmt.Println(decodeString(str))
+	p1 := []int{0, 0}
+	p2 := []int{1, 1}
+	p3 := []int{1, 0}
+	p4 := []int{0, 1}
+	fmt.Println(validSquare(p1, p2, p3, p4))
 }
 
 /*
 goal: decode string according to num[char]
-
-
 */
 
-func decodeString(s string) string {
+type Points struct {
+	x int
+	y int
+}
 
-	stack := []string{}
-	curr := ""
-	for _, val := range s {
-		if string(val) != "]" {
-			stack = append(stack, string(val)) //push
-		} else {
+func validSquare(p1 []int, p2 []int, p3 []int, p4 []int) bool {
 
-			for len(stack) > 0 {
-				//top
-				top := stack[len(stack)-1]
-				//pop
-				stack = stack[:len(stack)-1]
-				if top != "[" {
-					curr = top + curr
-				} else if top == "[" {
-					kStr := ""
-					for len(stack) > 0 {
-						// peek
-						n := len(stack) - 1
-						top := stack[n]
-						if _, err := strconv.Atoi(top); err == nil {
-							kStr = top + kStr
-							// pop
-							stack = stack[:n]
-						} else {
-							break
-						}
-					}
+	hash := make(map[int]bool, 2)
+	pp := make(map[[2]int]bool, 4)
 
-					i, _ := strconv.Atoi(kStr)
-					curr = strings.Repeat(curr, i)
-					stack = append(stack, curr)
-					curr = ""
-					break
-				}
+	p := [][]int{p1, p2, p3, p4}
+
+	for i, v := range p {
+		//fmt.Println(i)
+		fmt.Println("v", v)
+		pp[[2]int{v[0], v[1]}] = true
+		//fmt.Println("pp", pp)
+		for j := i + 1; j < len(p); j++ {
+			pp[[2]int{v[0], v[1]}] = true
+			//fmt.Println("pp2", pp)
+			fmt.Println("p[j][0]", p[j][0])
+			fmt.Println("v[0]", v[0])
+			dist := (p[j][0]-v[0])*(p[j][0]-v[0]) + (p[j][1]-v[1])*(p[j][1]-v[1])
+			fmt.Println("dist", dist)
+			hash[dist] = true
+
+			fmt.Println(hash)
+			if len(hash) > 2 {
+				return false
 			}
-			stack = append(stack, curr)
 		}
 	}
 
-	var res strings.Builder
-	for _, s := range stack {
-		res.WriteString(s)
-	}
-	return res.String()
-
+	return true
 }
