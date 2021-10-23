6709a947fd5634225b8db4f50edca47f864a263c
warrenbox <warren.veerasingam@gmail.com>
2021-10-22 23:04:43 -0500

    add

--- test/balanced-binary-tree/main.go
+++ test/balanced-binary-tree/main.go
@@ -123,3 +123,58 @@ func (this *CBTInserter) Insert(val int) int {
 func (this *CBTInserter) Get_root() *TreeNode {
 	return this.root
 }
+
+/*  BINARY TREE INSERTER */
+type CBTInserter struct {
+	root  *TreeNode
+	queue []*TreeNode
+}
+
+func Constructor(root *TreeNode) CBTInserter {
+
+	cbt := CBTInserter{root: root}
+
+	if root == nil {
+		return cbt
+	}
+
+	cbt.queue = append(cbt.queue, root)
+
+	for len(cbt.queue) != 0 {
+		root := cbt.queue[0]
+		if root.Left == nil {
+			return cbt
+		} else if root.Right == nil {
+			cbt.queue = append(cbt.queue, root.Left)
+			return cbt
+		}
+		cbt.queue = cbt.queue[1:]
+		cbt.queue = append(cbt.queue, root.Left, root.Right)
+
+	}
+	return cbt
+}
+
+func (this *CBTInserter) Insert(val int) int {
+
+	newNode := &TreeNode{Val: val}
+
+	if len(this.queue) == 0 {
+		this.root = newNode
+		this.queue = append(this.queue, newNode)
+		return -1
+	}
+	root := this.queue[0]
+	if root.Left == nil {
+		root.Left = newNode
+	} else {
+		root.Right = newNode
+		this.queue = this.queue[1:]
+	}
+	this.queue = append(this.queue, newNode)
+	return root.Val
+}
+
+func (this *CBTInserter) Get_root() *TreeNode {
+	return this.root
+}
